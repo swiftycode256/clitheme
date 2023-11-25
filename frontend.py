e8e74a4c36d41b312a85abfbd7f16b9e2c89de02
swiftycode <10752639+swiftycode@user.noreply.gitee.com>
2023-11-25 13:45:25 +0800

    First commit with frontend and generator completed

---
+++ frontend.py
@@ -0,0 +1,35 @@
+import os,sys
+
+data_path=os.environ["HOME"]+"/.local/clitheme/theme-data"
+class FetchDescriptor(self):
+    def __init__(self, domain_name, app_name, debug_mode=False):
+        self.domain_name=domain_name
+        self.app_name=app_name
+        self.debug_mode=debug_mode
+    def retrive_entry_or_fallback(self, entry_path, fallback_string):
+        # entry_path e.g. "class-a sample_text"
+        lang=""
+        lang_without_encoding=""
+        if os.environ.__contains__("LANG"):
+            lang=os.environ["LANG"]
+        if lang.strip()!="": # not empty
+            for char in lang:
+                if char=='.': # if reaches e.g. ".UTF-8" section
+                    break
+                lang_without_encoding+=char 
+        if self.debug_mode: print(lang, lang_without_encoding)
+        path=data_path+"/"+self.domain_name+"/"+self.app_name
+        for section in entry_path.split():
+            path+="/"+section
+        # path with lang, path with lang but without e.g. .UTF-8, path wth no lang
+        possible_paths=[path+"__"+lang, path+"__"+lang_without_encoding, path]
+        for p in possible_paths:
+            if self.debug_mode: print("Trying "+p, end="...")
+            try:
+                f=open(p,'r')
+                dat=f.read().strip()
+                if self.debug_mode: print("Success:\n"+dat)
+                return dat
+            except FileNotFoundError:
+                if self.debug_mode: print("Failed")
+        return fallback_string
