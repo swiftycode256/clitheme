d6e60247ad054a02f4e0eae6667463db4b0d810e
swiftycode <10752639+swiftycode@user.noreply.gitee.com>
2023-11-26 15:48:42 +0800

    Add version information; changed data path

--- _globalvar.py
+++ _globalvar.py
@@ -3,5 +3,7 @@ Global variable definitions for clitheme
 """
 
 import os
-clitheme_root_data_path=os.environ["HOME"]+"/.local/clitheme"
+
+clitheme_root_data_path=os.environ["HOME"]+"/.local/share/clitheme"
 clitheme_temp_root="/tmp"
+clitheme_version="1.0"
