1303bc10ab1c345a760a898399cdb0063b54403f
swiftycode <10752639+swiftycode@user.noreply.gitee.com>
2023-11-28 20:48:53 +0800

    Add disable_lang option

--- frontend.py
+++ frontend.py
@@ -13,17 +13,19 @@ class FetchDescriptor():
     """
     Object containing domain and app information used for fetching entries
     """
-    def __init__(self, domain_name="", app_name="", debug_mode=False):
+    def __init__(self, domain_name="", app_name="", debug_mode=False, disable_lang=False):
         """
         Create a new instance of the object.
         
         - Provide domain_name and app_name to automatically append them for retrival functions.
         - Set debug_mode=True to output underlying operations when retriving entries.
+        - Set disable_lang=True to disable localization detection and use "default" entry for all retrival operations
         """
         # Leave domain and app names blank for global reference
         self.domain_name=domain_name
         self.app_name=app_name
         self.debug_mode=debug_mode
+        self.disable_lang=disable_lang
     def retrive_entry_or_fallback(self, entry_path, fallback_string):
         """
         Attempt to retrieve the entry based on given entry path. 
@@ -44,15 +46,16 @@ class FetchDescriptor():
                         return fallback_string
         lang=""
         lang_without_encoding=""
-        if global_lang!="":
-            lang=global_lang
-        elif os.environ.__contains__("LANG"):
-            lang=os.environ["LANG"]
-        if lang.strip()!="": # not empty
-            for char in lang:
-                if char=='.': # if reaches e.g. ".UTF-8" section
-                    break
-                lang_without_encoding+=char 
+        if not self.disable_lang:
+            if global_lang!="":
+                lang=global_lang
+            elif os.environ.__contains__("LANG"):
+                lang=os.environ["LANG"]
+            if lang.strip()!="": # not empty
+                for char in lang:
+                    if char=='.': # if reaches e.g. ".UTF-8" section
+                        break
+                    lang_without_encoding+=char 
         if self.debug_mode: print("[Debug]", lang, lang_without_encoding, entry_path)
         path=data_path+"/"+self.domain_name+"/"+self.app_name
         for section in entry_path.split():
