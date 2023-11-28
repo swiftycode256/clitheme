1b0555a1cfa4243bebf14e6420c48df673ccb247
swiftycode <10752639+swiftycode@user.noreply.gitee.com>
2023-11-28 22:03:42 +0800

    Add HOME variable sanity checking

--- _globalvar.py
+++ _globalvar.py
@@ -4,7 +4,17 @@ Global variable definitions for clitheme
 
 import os
 
-clitheme_root_data_path=os.environ["HOME"]+"/.local/share/clitheme"
+clitheme_root_data_path=""
+
+try: 
+    if not os.environ['HOME'].startswith("/"): # sanity check
+        raise KeyError
+    clitheme_root_data_path=os.environ["HOME"]+"/.local/share/clitheme"
+except KeyError:
+    print("Error: unable to get your home directory or invaild home directory information")
+    print("Please make sure that the $HOME environment variable is set correctly.")
+    print("Try restarting your terminal session to fix this issue.")
+    exit(1)
 clitheme_temp_root="/tmp"
 clitheme_version="1.0-dev"
 entry_banphrases=['/','\\']
