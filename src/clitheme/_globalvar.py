f3401282dda446c80d2d6b82cee340c510610e00
swiftycode <10752639+swiftycode@user.noreply.gitee.com>
2023-11-29 13:26:12 +0800

    Change version file location

--- src/clitheme/_globalvar.py
+++ src/clitheme/_globalvar.py
@@ -3,6 +3,8 @@ Global variable definitions for clitheme
 """
 
 import os
+try: from . import _version
+except ImportError: import _version
 
 clitheme_root_data_path=""
 
@@ -16,7 +18,7 @@ except KeyError:
     print("Try restarting your terminal session to fix this issue.")
     exit(1)
 clitheme_temp_root="/tmp"
-clitheme_version="1.0-dev"
+clitheme_version=_version.__version__
 generator_info_pathname="theme-info" # e.g. ~/.local/share/clitheme/theme-info
 generator_data_pathname="theme-data" # e.g. ~/.local/share/clitheme/theme-data
 entry_banphrases=['/','\\']
---
+++ src/clitheme/_version.py
@@ -0,0 +1,2 @@
+# Version definition file; define the package version here
+__version__="1.0-dev"
