285fbe24743ffb5ec457da38feb655eb4574f218
swiftycode <10752639+swiftycode@user.noreply.gitee.com>
2023-11-29 19:45:45 +0800

    Add major and relnumber to version.py

--- src/clitheme/_version.py
+++ src/clitheme/_version.py
@@ -1,2 +1,6 @@
 # Version definition file; define the package version here
+# Version CANNOT contain hyphens (-); use underscores (_) instead
 __version__="1.0_dev"
+# For PKGBUILD
+version_major="1.0_dev"
+version_relnumber=1
