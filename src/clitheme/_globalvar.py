6f4c79196632053e161574014c2a93287393a30e
swiftycode <10752639+swiftycode@user.noreply.gitee.com>
2023-11-30 19:32:08 +0800

    Add sanity_check function to _globalvar

--- src/clitheme/_globalvar.py
+++ src/clitheme/_globalvar.py
@@ -23,3 +23,17 @@ generator_info_pathname="theme-info" # e.g. ~/.local/share/clitheme/theme-info
 generator_data_pathname="theme-data" # e.g. ~/.local/share/clitheme/theme-data
 generator_index_filename="current_theme_index"
 entry_banphrases=['/','\\']
+# function to check whether the pathname contains invaild phrases
+# - cannot start with .
+# - cannot contain banphrases
+sanity_check_error_message=""
+def sanity_check(path):
+    for p in path.split():
+        if p.startswith('.'):
+            sanity_check_error_message="cannot start with '.'"
+            return False
+        for b in entry_banphrases:
+            if p.find(b)!=-1:
+                sanity_check_error_message="cannot contain '{}'".format(b)
+                return False
+    return True
