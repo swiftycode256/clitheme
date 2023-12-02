dbaea00de5759a3eb99da91d8dfa4f7aa8d58137
swiftycode <10752639+swiftycode@user.noreply.gitee.com>
2023-12-02 14:29:11 +0800

    Update no $HOME message

--- src/clitheme/_globalvar.py
+++ src/clitheme/_globalvar.py
@@ -13,7 +13,7 @@ try:
         raise KeyError
     clitheme_root_data_path=os.environ["HOME"]+"/.local/share/clitheme"
 except KeyError:
-    print("Error: unable to get your home directory or invaild home directory information")
+    print("[clitheme] Error: unable to get your home directory or invaild home directory information")
     print("Please make sure that the $HOME environment variable is set correctly.")
     print("Try restarting your terminal session to fix this issue.")
     exit(1)
