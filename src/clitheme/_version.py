787e120ff0e1b46619312ec29070a82e322d42fa
swiftycode <10752639+swiftycode@user.noreply.gitee.com>
2023-12-08 18:28:34 +0800

    Add individual information to version file

--- src/clitheme/_version.py
+++ src/clitheme/_version.py
@@ -2,6 +2,9 @@
 # Version CANNOT contain hyphens (-); use underscores (_) instead
 # The __version__ variable must be a literal string; DO NOT use variables
 __version__="1.0_dev"
+major=1
+minor=0
+release=0 # 0 stands for "dev"
 # For PKGBUILD
 version_main=__version__
 version_buildnumber=1
