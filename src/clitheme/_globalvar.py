e0a25d45d665614239a2e6c77c714b37b2ea7bea
swiftycode <10752639+swiftycode@user.noreply.gitee.com>
2023-12-11 11:23:14 +0800

    Add XDG_DATA_HOME support

--- src/clitheme/_globalvar.py
+++ src/clitheme/_globalvar.py
@@ -7,16 +7,20 @@ try: from . import _version
 except ImportError: import _version
 
 clitheme_root_data_path=""
+try:
+    clitheme_root_data_path=os.environ["XDG_DATA_HOME"]+"/clitheme"
+except KeyError: None
 
-try: 
-    if not os.environ['HOME'].startswith("/"): # sanity check
-        raise KeyError
-    clitheme_root_data_path=os.environ["HOME"]+"/.local/share/clitheme"
-except KeyError:
-    print("[clitheme] Error: unable to get your home directory or invaild home directory information")
-    print("Please make sure that the $HOME environment variable is set correctly.")
-    print("Try restarting your terminal session to fix this issue.")
-    exit(1)
+if clitheme_root_data_path=="": # prev did not succeed
+    try: 
+        if not os.environ['HOME'].startswith("/"): # sanity check
+            raise KeyError
+        clitheme_root_data_path=os.environ["HOME"]+"/.local/share/clitheme"
+    except KeyError:
+        print("[clitheme] Error: unable to get your home directory or invaild home directory information")
+        print("Please make sure that the $HOME environment variable is set correctly.")
+        print("Try restarting your terminal session to fix this issue.")
+        exit(1)
 clitheme_temp_root="/tmp"
 clitheme_version=_version.__version__
 generator_info_pathname="theme-info" # e.g. ~/.local/share/clitheme/theme-info
