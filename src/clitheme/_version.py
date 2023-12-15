4727a620051f5d61cee633f5cb77cad91cf66286
swiftycode <10752639+swiftycode@user.noreply.gitee.com>
2023-12-15 12:36:51 +0800

    Change version to 1.0-r2

--- src/clitheme/_version.py
+++ src/clitheme/_version.py
@@ -1,10 +1,10 @@
 # Version definition file; define the package version here
 # Version CANNOT contain hyphens (-); use underscores (_) instead
 # The __version__ variable must be a literal string; DO NOT use variables
-__version__="1.0_r1"
+__version__="1.0-r2"
 major=1
 minor=0
-release=1 # 0 stands for "dev"
+release=2 # 0 stands for "dev"
 # For PKGBUILD
-version_main=__version__
+version_main="1.0_r2"
 version_buildnumber=1
