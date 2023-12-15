03cab4de65b18dded9e3f3a86da482e0bac3cf92
swiftycode <10752639+swiftycode@user.noreply.gitee.com>
2023-12-15 13:15:50 +0800

    Upload debian files

---
+++ debian/upstream/metadata.ex
@@ -0,0 +1,10 @@
+# Example file for upstream/metadata.
+# See https://wiki.debian.org/UpstreamMetadata for more info/fields.
+# Below an example based on a github project.
+
+Bug-Database: https://gitee.com/swiftycode/clitheme/issues
+Bug-Submit: https://gitee.com/swiftycode/clitheme/issues/new
+# Changelog: https://gitee.com/swiftycode/clitheme/blob/master/CHANGES
+Documentation: https://gitee.com/swiftycode/clitheme/wiki
+Repository-Browse: https://gitee.com/swiftycode/clitheme
+Repository: https://gitee.com/swiftycode/clitheme.git
