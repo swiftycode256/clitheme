b0de90b0fe2e10becdf1d62c8077e596a8077292
swiftycode <10752639+swiftycode@user.noreply.gitee.com>
2024-04-09 21:52:31 +0800

    Implement strictcmdmatch, exactcmdmatch, and endmatchhere in db_interface

--- src/clitheme/_generator/db_interface.py
+++ src/clitheme/_generator/db_interface.py
@@ -1,6 +1,7 @@
 import sys
 import sqlite3
 import re
+import copy
 from typing import Optional
 try: from .. import _globalvar
 except ImportError: import _globalvar
@@ -10,15 +11,18 @@ def init_db(file_path: str):
     global connection
     connection=sqlite3.connect(file_path)
     # create the table
+    # command_match_strictness: 0:default match options, 1:must start with pattern, 2: must exactly equal pattern
     connection.execute(f"CREATE TABLE {_globalvar.db_data_tablename} ( \
                     match_pattern TEXT NOT NULL, \
                     substitute_pattern TEXT NOT NULL, \
                     is_regex INTEGER DEFAULT true NOT NULL, \
-                    effective_command TEXT \
+                    effective_command TEXT, \
+                    command_match_strictness INTEGER DEFAULT 0 NOT NULL, \
+                    end_match_here INTEGER DEFAULT 0 NOT NULL \
                     );")
     connection.commit()
 
-def add_subst_entry(match_pattern: str, substitute_pattern: str, effective_commands: Optional[list[str]], is_regex: bool=True):
+def add_subst_entry(match_pattern: str, substitute_pattern: str, effective_commands: Optional[list[str]], is_regex: bool=True, command_match_strictness: int=0, end_match_here: bool=False, line_number_debug: int=-1):
     global cursor
     cmdlist=[]
     if effective_commands!=None and len(effective_commands)>0: 
@@ -26,51 +30,79 @@ def add_subst_entry(match_pattern: str, substitute_pattern: str, effective_comma
             # remove extra spaces in the command
             cmdlist.append(re.sub(r" {2,}", " ", cmd).strip())
     else:
+        # remove any existing values with the same match_pattern
         if len(connection.execute(f"SELECT * FROM {_globalvar.db_data_tablename} WHERE match_pattern=? AND typeof(effective_command)=typeof(null);", (match_pattern.strip(),)).fetchall())>0:
-            print("Warning: Repeated entry at line %, overwriting")
+            print(f"Warning: Repeated entry at line {line_number_debug}, overwriting")
             connection.execute(f"DELETE FROM {_globalvar.db_data_tablename} WHERE match_pattern=? AND typeof(effective_command)=typeof(null);", (match_pattern.strip(),))
         # insert the entry into the main table
-        connection.execute(f"INSERT INTO {_globalvar.db_data_tablename} (match_pattern, substitute_pattern, is_regex) VALUES (?,?,?);", (match_pattern.strip(), substitute_pattern.strip(), is_regex))
+        connection.execute(f"INSERT INTO {_globalvar.db_data_tablename} (match_pattern, substitute_pattern, is_regex, command_match_strictness, end_match_here) VALUES (?,?,?,?,?);", (match_pattern.strip(), substitute_pattern.strip(), is_regex, command_match_strictness, end_match_here))
     for cmd in cmdlist:
-        # remove any existing values with the match_pattern and effective_command, if any
-        if len(connection.execute(f"SELECT * FROM {_globalvar.db_data_tablename} WHERE match_pattern=? AND effective_command=?;", (match_pattern.strip(),cmd)).fetchall())>0:
-            print("Warning: Repeated entry at line %, overwriting")
-            connection.execute(f"DELETE FROM {_globalvar.db_data_tablename} WHERE match_pattern=? AND effective_command=?;", (match_pattern.strip(),cmd))
+        # remove any existing values with the same match_pattern and effective_command and command_match_strictness(if ==2)
+        extra_condition=""
+        if command_match_strictness==2: extra_condition=" AND command_match_strictness=2"
+        if len(connection.execute(f"SELECT * FROM {_globalvar.db_data_tablename} WHERE match_pattern=? AND effective_command=?{extra_condition};", (match_pattern.strip(),cmd)).fetchall())>0:
+            print(f"Warning: Repeated entry at line {line_number_debug}, overwriting")
+            connection.execute(f"DELETE FROM {_globalvar.db_data_tablename} WHERE match_pattern=? AND effective_command=?{extra_condition};", (match_pattern.strip(),cmd))
         # insert the entry into the main table
-        connection.execute(f"INSERT INTO {_globalvar.db_data_tablename} (match_pattern, substitute_pattern, effective_command, is_regex) VALUES (?,?,?,?);", (match_pattern.strip(), substitute_pattern.strip(), cmd, is_regex))
+        connection.execute(f"INSERT INTO {_globalvar.db_data_tablename} (match_pattern, substitute_pattern, effective_command, is_regex, command_match_strictness, end_match_here) VALUES (?,?,?,?,?,?);", (match_pattern.strip(), substitute_pattern.strip(), cmd, is_regex, command_match_strictness, end_match_here))
     connection.commit()
 
 def match_content(content: bytes, command: Optional[str]=None) -> bytes:
+    # Match order:
+    # 1. Match rules with exactcmdmatch option set
+    # 2. Match rules with command filter having the same first phrase
+    #   - Command filters with greater number of phrases are prioritized over others
+    # 3. Match rules without command filter
+
     # retrieve a list of effective commands matching first argument
-    target_command=None
+    final_cmdlist=[]
+    final_cmdlist_strictmatch=[]
     if command!=None and len(command.split())>0:
-        cmdlist=connection.execute(f"SELECT DISTINCT effective_command FROM {_globalvar.db_data_tablename} WHERE effective_command LIKE ?;", (command.split()[0].strip()+" %",)).fetchall()
-        # also include one-phrase effective commands
-        cmdlist+=connection.execute(f"SELECT DISTINCT effective_command FROM {_globalvar.db_data_tablename} WHERE effective_command=?;", (command.split()[0].strip(),)).fetchall()
-        # sort by command length (greatest to least)
-        cmdlist.sort(key=len, reverse=True)
+        # obtain a list of effective_command with the same first term
+        cmdlist=connection.execute(f"SELECT DISTINCT effective_command, command_match_strictness FROM {_globalvar.db_data_tablename} WHERE effective_command LIKE ?;", (command.split()[0].strip()+" %",)).fetchall()
+        # also include one-phrase commands
+        cmdlist+=connection.execute(f"SELECT DISTINCT effective_command, command_match_strictness FROM {_globalvar.db_data_tablename} WHERE effective_command=?;", (command.split()[0].strip(),)).fetchall()
+        # sort by number of phrases (greatest to least)
+        def split_len(obj: tuple) -> int: return len(obj[0].split())
+        cmdlist.sort(key=split_len, reverse=True)
+        # prioritize effective_command with exact match requirement
+        cmdlist=connection.execute(f"SELECT DISTINCT effective_command, command_match_strictness FROM {_globalvar.db_data_tablename} WHERE effective_command=? AND command_match_strictness=2", (command.strip(),)).fetchall()+cmdlist
         # attempt to find matching command 
         for tp in cmdlist:
             cmd=tp[0] # extract value from tuple
+            strictness=tp[1] # strictness setting
             success=True
-            for phrase in cmd.split():
-                if phrase not in command.split():
-                    success=False; break
+            if strictness==1: # must start with pattern
+                if not command.startswith(cmd): success=False
+            elif strictness==2: # must equal to pattern
+                if not command==cmd: success=False
+            else: # implying strictness==0; must contain all phrases in pattern
+                for phrase in cmd.split():
+                    if phrase not in command.split():
+                        success=False; break
             if success:
                 # if found matching command
-                target_command=cmd
+                if cmd not in final_cmdlist: final_cmdlist.append(cmd)
+                final_cmdlist_strictmatch.append(strictness==2)
                 break
-    content_str=content
+    content_str=copy.copy(content)
     matches=[]
-    if target_command!=None: matches+=connection.execute(f"SELECT match_pattern, substitute_pattern, is_regex FROM {_globalvar.db_data_tablename} WHERE effective_command=? ORDER BY rowid;", (target_command,)).fetchall()
-    matches+=connection.execute(f"SELECT match_pattern, substitute_pattern, is_regex FROM {_globalvar.db_data_tablename} WHERE typeof(effective_command)=typeof(null) ORDER BY rowid;").fetchall()
+    if len(final_cmdlist)>0:
+        for x in range(len(final_cmdlist)):
+            cmd=final_cmdlist[x]
+            # prioritize exact match
+            if final_cmdlist_strictmatch[x]==True: matches+=connection.execute(f"SELECT match_pattern, substitute_pattern, is_regex, end_match_here FROM {_globalvar.db_data_tablename} WHERE effective_command=? AND command_match_strictness=2 ORDER BY rowid;", (cmd,)).fetchall()
+            matches+=connection.execute(f"SELECT match_pattern, substitute_pattern, is_regex, end_match_here FROM {_globalvar.db_data_tablename} WHERE effective_command=? ORDER BY rowid;", (cmd,)).fetchall()
+    matches+=connection.execute(f"SELECT match_pattern, substitute_pattern, is_regex, end_match_here FROM {_globalvar.db_data_tablename} WHERE typeof(effective_command)=typeof(null) ORDER BY rowid;").fetchall()
     for match_data in matches:
         try:
             if match_data[2]==True: # is regex 
                 content_str=re.sub(bytes(match_data[0],'utf-8'), bytes(match_data[1], 'utf-8'), content_str)
-            else:
+            else: # is string
                 content_str=content_str.replace(bytes(match_data[0],'utf-8'), bytes(match_data[1],'utf-8'))
         except:
             print("Error occurred while matching string: ", end="")
             print(sys.exc_info()[1])
+        if match_data[3]==True: # endmatchoption is set
+            break
     return content_str
--- src/db_interface_tests.py
+++ src/db_interface_tests.py
@@ -12,20 +12,27 @@ sample_inputs=[("rm: missing operand", "rm"),
                ("ls: unrecognized option '--help'", "ls --help"),
                ("Warning: invaild input", "input anything"),
                ("Error: invaild input   ","input anything"), # test extra spaces
+               ("Error: sample message", "example_app --this install-stuff"), # test strictcmdmatch (substitution should not happen)
+               ("Error: sample message", "example_app install-stuff --this"), # test strictcmdmatch and endmatchhere options
+               ("rm: <no filename>: Permission denied", "rm -rf") # test exactcmdmatch (substitution rule containing this option should be prioritized over previous rules)
 ]
 # substitute patterns
-subst_patterns=[("rm: missing operand", "rm says: missing arguments and options (>﹏<)", False, ["rm"]),
-                ("type rm --help for more information", "For more information, use rm --help (｡ì _ í｡)", False, ["rm"]),
-                (r"(?P<shell>.+): (?P<filename>.+): Permission denied",r"""\g<shell> says: Access denied to \g<filename>! ಥ_ಥ""",True, ["rm -rf", "cat", "cd", "ls"]),
-                (r"(?P<shell>.+): unrecognized option '(?P<opt>.+)'",r"""wef""",True, ["ls"]), # testing repeated entry detection
-                (r"(?P<shell>.+): unrecognized option '(?P<opt>.+)'",r"""\g<shell> says: option '\g<opt>' not known! (ToT)/~~~'""",True, ["ls"]),
-                (r"^Warning:( )", r"o(≧v≦)o Note:\g<1>",True , None),
-                (r"^Error:( )", r"(ToT)/~~~ Error:\g<1>",True, None),
-                (r"invaild input( ){0,}$", r"input is invaild! ಥ_ಥ", True, None)]
+# (match_pattern, substitute_pattern, is_regex, effective_commands, command_match_strictness, end_match_here)
+subst_patterns=[("rm: missing operand", "rm says: missing arguments and options (>﹏<)", False, ["rm"], 0, False),
+                ("type rm --help for more information", "For more information, use rm --help (｡ì _ í｡)", False, ["rm"], 0, False),
+                (r"(?P<shell>.+): (?P<filename>.+): Permission denied",r"""\g<shell> says: Access denied to \g<filename>! ಥ_ಥ""",True, ["rm -rf", "cat", "cd", "ls"], 0, False),
+                (r"(?P<shell>.+): unrecognized option '(?P<opt>.+)'",r"""wef""",True, ["ls"], 0, False), # testing repeated entry detection
+                (r"(?P<shell>.+): unrecognized option '(?P<opt>.+)'",r"""\g<shell> says: option '\g<opt>' not known! (ToT)/~~~'""",True, ["ls"], 0, False),
+                (r"^Warning:( )", r"o(≧v≦)o Note:\g<1>",True , None, 0, False),
+                (r"^Error:( )", r"(ToT)/~~~ Error:\g<1>",True, None, 0, False),
+                (r"invaild input( ){0,}$", r"input is invaild! ಥ_ಥ", True, None, 0, False),
+                ("Error: sample message", "Error: sample message! (>﹏<)", False, ["example_app install-stuff"], 1, True),
+                (r"(?P<shell>.+): (?P<filename>.+): Permission denied",r"""\g<shell> says: Missing argument for operation! ಥ_ಥ""",True, ["rm -rf"], 2, False),
+]
 
 db_interface.init_db(":memory:")
 # record substitute patterns
-for dat in subst_patterns: db_interface.add_subst_entry(match_pattern=dat[0], substitute_pattern=dat[1], effective_commands=dat[3], is_regex=dat[2])
+for dat in subst_patterns: db_interface.add_subst_entry(match_pattern=dat[0], substitute_pattern=dat[1], effective_commands=dat[3], is_regex=dat[2], command_match_strictness=dat[4], end_match_here=dat[5])
 print("Successfully recorded data\nTesting sample outputs: ")
 for inp in sample_inputs:
     print(db_interface.match_content(bytes(inp[0],'utf-8'),command=inp[1]).decode('utf-8'))
