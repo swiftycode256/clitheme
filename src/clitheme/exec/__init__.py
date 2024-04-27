dc4d75cfbc39528cacf0d14e410fa6b71b21dc77
swiftycode <10752639+swiftycode@user.noreply.gitee.com>
2024-04-27 14:48:45 +0800

    Implement debug mode in clitheme-exec

--- src/clitheme/exec/__init__.py
+++ src/clitheme/exec/__init__.py
@@ -14,12 +14,26 @@ def main(arguments: list[str]):
     if len(arguments)<2: 
         print("Not enough arguments")
         return 1
-    # check dataase
+    # check database
     if not os.path.exists(f"{_globalvar.clitheme_root_data_path}/{_globalvar.db_filename}"):
         print("Warning: no theme set or theme does not have substrules")
+    # process debug mode arguments
+    debug_mode=[]
+    argcount=0
+    for arg in arguments[1:]:
+        if not arg.startswith('-'): break
+        argcount+=1
+        if arg=="--debug":
+            debug_mode.append("normal")
+        elif arg=="--debug-color":
+            debug_mode.append("color")
+        elif arg=="--debug-newlines":
+            debug_mode.append("newlines")
+        elif arg=="--debug-showchars":
+            debug_mode.append("showchars")
     # determine platform
     if os.name=="posix":
-        output_handler_posix.handler_main(arguments[1:])
+        output_handler_posix.handler_main(arguments[1+argcount:], debug_mode)
     elif os.name=="nt":
         print("Windows platform is not currently supported")
         return 1
--- src/clitheme/exec/output_handler_posix.py
+++ src/clitheme/exec/output_handler_posix.py
@@ -3,9 +3,10 @@ import sys
 import os
 import pty
 import select
-import termios, tty
-import time
+import termios
 import copy
+import string
+from typing import Optional
 try:
     from .._generator import db_interface
     from .. import _globalvar
@@ -14,7 +15,30 @@ except ImportError:
     import _globalvar
 
 
-def handler_main(command: list[str]):
+def process_debug(lines: list[bytes], debug_mode: list[str], is_stderr: bool=False, matched: bool=False) -> list[bytes]:
+    # debug_mode: newlines, showchars, color
+    final_lines=[]
+    for x in range(len(lines)):
+        line=lines[x]
+        if "newlines" in debug_mode:
+            if not line.endswith(b'\n') and not line.endswith(b'\f') and not line.endswith(b'\x0c'):
+                line+=b"\n"
+        if "showchars" in debug_mode:
+            wrapper=b"\x1b[32m{}\x1b[0m"
+            if "color" in debug_mode: wrapper+=bytes(f"\x1b[{'31' if is_stderr else '33'}m", 'utf-8')
+            line=line.replace(b'\x1b', wrapper.replace(b'{}', b'{{ESC}}')) # this must come before anything else
+            line=line.replace(b'\r', wrapper.replace(b'{}',b'\\r'))
+            line=line.replace(b'\b', wrapper.replace(b'{}',b'\\x08'))
+            line=line.replace(b'\a', wrapper.replace(b'{}',b'\\x07'))
+        if "color" in debug_mode:
+            line=bytes(f"\x1b[{'31' if is_stderr else '33'}m", 'utf-8')+line+b"\x1b[0m"
+        if "normal" in debug_mode:
+            # e.g. o{ <line>; o> <start>
+            line=bytes(f"\x1b[0;1;{'31' if is_stderr else '32'}{';47' if matched else ''}m"+('e' if is_stderr else 'o')+'\x1b[0;1m'+(">")+"\x1b[0m ",'utf-8')+line+b"\x1b[0m"
+        final_lines.append(line)
+    return final_lines
+
+def handler_main(command: list[str], debug_mode: list[str]=[]):
     do_subst=True
     try: db_interface.connect_db()
     except FileNotFoundError: do_subst=False
@@ -34,7 +58,7 @@ def handler_main(command: list[str]):
             termios.tcsetattr(sys.stdin, termios.TCSADRAIN, termios.tcgetattr(stdin_fd))
             fds=select.select([stdout_fd, sys.stdin, stderr_fd], [], [], 0.1)[0]
             output_lines=[] # (line_content, is_stderr)
-            readsize=40960
+            readsize=1000000
             if sys.stdin in fds:
                 data=os.read(sys.stdin.fileno(), readsize)
                 if not data: break
@@ -43,13 +67,15 @@ def handler_main(command: list[str]):
                 data=os.read(stdout_fd, readsize)
                 #data=b'\x1b[33m'+data.replace(b'\x1b',b'\x1b[32m{{ESC}}\x1b[33m')+b'\x1b[0m' # DEBUG purposes
                 #data=b'\x1b[33m'+data+b'\x1b[0m' # DEBUG purposes
-                for line in data.splitlines():
+                lines=data.splitlines(keepends=True)
+                for line in lines:
                     output_lines.append((line,False))
             if stderr_fd in fds:
                 data=os.read(stderr_fd, readsize)
                 #data=b'\x1b[31m'+data.replace(b'\x1b',b'\x1b[32m{{ESC}}\x1b[31m')+b'\x1b[0m' # DEBUG purposes
                 #data=b'\x1b[31m'+data+b'\x1b[0m' # DEBUG purposes
-                for line in data.splitlines():
+                lines=data.splitlines(keepends=True)
+                for line in lines:
                     output_lines.append((line,True))
             # Process outputs
             for line_data in output_lines:
@@ -57,7 +83,8 @@ def handler_main(command: list[str]):
                 # subst operation
                 subst_line=copy.copy(line)
                 if do_subst: subst_line=db_interface.match_content(line, _globalvar.splitarray_to_string(command), is_stderr=line_data[1])
-                os.write(sys.stderr.fileno() if line_data[1]==True else sys.stdout.fileno(), subst_line+b"\n")
+                subst_line=process_debug([subst_line], debug_mode, is_stderr=line_data[1], matched=not subst_line==line)[0] 
+                os.write(sys.stderr.fileno() if line_data[1]==True else sys.stdout.fileno(), subst_line)
         except KeyboardInterrupt:
             process.send_signal(2) #SIGINT
             #os.write(stdin_fd, b'\x03')
