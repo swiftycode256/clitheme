aa69dca558ab2bbd1fe60e3a022f9b7446a79da2
swiftycode <10752639+swiftycode@user.noreply.gitee.com>
2024-05-01 15:24:55 +0800

    Update version (v2.0-dev20240429)

--- src/clitheme/_version.py
+++ src/clitheme/_version.py
@@ -1,10 +1,10 @@
 # Version definition file; define the package version here
 # The __version__ variable must be a literal string; DO NOT use variables
-__version__="2.0-dev20240429"
+__version__="2.0-dev20240430"
 major=2
 minor=0
 release=0 # 0 stands for "dev"
 # For PKGBUILD
 # version_main CANNOT contain hyphens (-); use underscores (_) instead
-version_main="2.0_dev20240429"
+version_main="2.0_dev20240430"
 version_buildnumber=1
