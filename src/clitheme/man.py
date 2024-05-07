c66b3c82b63bde8f21308a5c63df7274eada95e2
swiftycode <10752639+swiftycode@user.noreply.gitee.com>
2024-05-07 13:58:01 +0800

    First version of clitheme-man

---
+++ src/clitheme/man.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+import sys
+import os
+import subprocess
+import shutil
+try: from . import _globalvar
+except ImportError: import _globalvar
+
+def main(args: list[str]):
+    if os.name=="nt":
+        print("Windows platform not supported")
+        return 1
+    # check if "man" exists on system
+    man_executable: str=shutil.which("man") # type: ignore
+    if man_executable==None:
+        print("Error: \"man\" is not installed on this system")
+        return 1
+    env=os.environ
+    # check if theme is set
+    if not os.path.exists(f"{_globalvar.clitheme_root_data_path}/{_globalvar.generator_manpage_pathname}"):
+        print("Warning: no theme set or theme does not contain manpages")
+    # set MANPATH
+    env['MANPATH']=_globalvar.clitheme_root_data_path+"/"+_globalvar.generator_manpage_pathname+":"+(os.environ['MANPATH'] if 'MANPATH' in os.environ else '')
+    # invoke man
+    results=subprocess.run([man_executable]+args[1:], env=env)
+    return results.returncode
+
+def script_main(): # for script
+    return main(sys.argv)
+if __name__=="__main__":
+    exit(main(sys.argv))
