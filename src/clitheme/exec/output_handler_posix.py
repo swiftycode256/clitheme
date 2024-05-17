b367efed6f8b929e308a9846d2256e90a225dd48
swiftycode <10752639+swiftycode@user.noreply.gitee.com>
2024-05-18 00:12:26 +0800

    Reset color at every end of newline in --debug-color

--- src/clitheme/exec/output_handler_posix.py
+++ src/clitheme/exec/output_handler_posix.py
@@ -39,6 +39,7 @@ def _process_debug(lines: list[bytes], debug_mode: list[str], is_stderr: bool=Fa
             sub_pattern=f"\\g<0>\x1b[{'31' if is_stderr else '33'}m"
             try: line=bytes(re.sub(match_pattern, sub_pattern, line.decode('utf-8')), 'utf-8')
             except UnicodeDecodeError: line=re.sub(bytes(match_pattern, 'utf-8'), bytes(sub_pattern, 'utf-8'), line)
+            line+=b'\x1b[0m'
         if "normal" in debug_mode:
             # e.g. o{ <line>; o> <start>
             line=bytes(f"\x1b[0;1;{'31' if is_stderr else '32'}{';47' if matched else ''}m"+('e' if is_stderr else 'o')+'\x1b[0;1m'+(">")+"\x1b[0m ",'utf-8')+line+b"\x1b[0m"
