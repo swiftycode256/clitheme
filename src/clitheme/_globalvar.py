5d9bb23f127122c71be2347be5ef2996ccbcce38
swiftycode <10752639+swiftycode@user.noreply.gitee.com>
2024-06-04 15:32:42 +0800

    Add example to db_filename definition

--- src/clitheme/_globalvar.py
+++ src/clitheme/_globalvar.py
@@ -57,7 +57,7 @@ generator_info_v2filename=generator_info_filename+"_v2" # e.g. [...]/theme-info/
 
 ## _generator.db_interface file and table names
 db_data_tablename="clitheme_subst_data"
-db_filename="subst-data.db"
+db_filename="subst-data.db" # e.g. ~/.local/share/clitheme/subst-data.db
 db_version=2
 
 ## Sanity check function
