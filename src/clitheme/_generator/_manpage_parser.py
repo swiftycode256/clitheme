453dd559689fe065610d70eda99a26f9d3593ae8
swiftycode <10752639+swiftycode@user.noreply.gitee.com>
2024-07-10 21:51:28 +0800

    Add support for multiple `[file_content]` directive specification and `[include_file]` block;
    - Fixes an issue where `{manpage_section}` is not checked for invalid phrases

--- src/clitheme/_generator/_manpage_parser.py
+++ src/clitheme/_generator/_manpage_parser.py
@@ -20,30 +20,49 @@ def handle_manpage_section(obj: _dataclass.GeneratorObject, first_phrase: str):
     end_phrase="{/manpage_section}"
     while obj.goto_next_line():
         phrases=obj.lines_data[obj.lineindex].split()
-        if phrases[0]=="[file_content]":
-            obj.check_enough_args(phrases, 2)
-            filepath=obj.subst_variable_content(_globalvar.splitarray_to_string(phrases[1:])).split()
-            # sanity check the file path
-            if _globalvar.sanity_check(_globalvar.splitarray_to_string(filepath))==False:
-                obj.handle_error(obj.fd.feof("sanity-check-manpage-err", "Line {num}: manpage paths {sanitycheck_msg}; use spaces to denote subdirectories", num=str(obj.lineindex+1), sanitycheck_msg=_globalvar.sanity_check_error_message))
-            content=obj.handle_block_input(preserve_indents=True, preserve_empty_lines=True, end_phrase="[/file_content]")
-            obj.write_manpage_file(filepath, content, obj.lineindex+1)
-        elif phrases[0]=="include_file":
-            obj.check_enough_args(phrases, 2)
-            filepath=obj.subst_variable_content(_globalvar.splitarray_to_string(phrases[1:])).split()
-            if _globalvar.sanity_check(_globalvar.splitarray_to_string(filepath))==False:
-                obj.handle_error(obj.fd.feof("sanity-check-manpage-err", "Line {num}: manpage paths {sanitycheck_msg}; use spaces to denote subdirectories", num=str(obj.lineindex+1), sanitycheck_msg=_globalvar.sanity_check_error_message))
-            # get content
-            # if no filename provided, use current working directory as parent path; else, use the directory the file is in as the parent path
+        def get_file_content(filepath: list[str]) -> str:
+            # determine file path
             parent_dir=""
+            # if no filename provided, use current working directory as parent path; else, use the directory the file is in as the parent path
             if obj.filename.strip()!="":
                 parent_dir+=os.path.dirname(obj.filename)
             file_dir=parent_dir+("/" if parent_dir!="" else "")+_globalvar.splitarray_to_string(filepath).replace(" ","/")
+            # get file content
             filecontent: str
             try: filecontent=open(file_dir, 'r', encoding="utf-8").read()
             except: obj.handle_error(obj.fd.feof("include-file-read-error", "Line {num}: unable to read file \"{filepath}\":\n{error_msg}", num=str(obj.lineindex+1), filepath=obj.fmt(file_dir), error_msg=sys.exc_info()[1]))
             # write manpage files in theme-info for db migration feature to work successfully
             obj.write_manpage_file(filepath, filecontent, obj.lineindex+1, custom_parent_path=obj.path+"/"+_globalvar.generator_info_pathname+"/"+obj.custom_infofile_name+"/manpage_data")
+            return filecontent
+        if phrases[0]=="[file_content]":
+            def handle(p: list[str]) -> list[str]:
+                obj.check_enough_args(p, 2)
+                filepath=obj.subst_variable_content(_globalvar.splitarray_to_string(p[1:])).split()
+                # sanity check the file path
+                if _globalvar.sanity_check(_globalvar.splitarray_to_string(filepath))==False:
+                    obj.handle_error(obj.fd.feof("sanity-check-manpage-err", "Line {num}: manpage paths {sanitycheck_msg}; use spaces to denote subdirectories", num=str(obj.lineindex+1), sanitycheck_msg=_globalvar.sanity_check_error_message))
+                return filepath
+            file_paths=[handle(phrases)]
+            # handle additional [file_content] phrases
+            prev_line_index=obj.lineindex
+            while obj.goto_next_line():
+                p=obj.lines_data[obj.lineindex].split()
+                if p[0]=="[file_content]":
+                    prev_line_index=obj.lineindex
+                    file_paths.append(handle(p))
+                else:
+                    obj.lineindex=prev_line_index
+                    break
+            content=obj.handle_block_input(preserve_indents=True, preserve_empty_lines=True, end_phrase="[/file_content]")
+            for filepath in file_paths:
+                obj.write_manpage_file(filepath, content, obj.lineindex+1)
+        elif phrases[0]=="include_file":
+            obj.check_enough_args(phrases, 2)
+            filepath=obj.subst_variable_content(_globalvar.splitarray_to_string(phrases[1:])).split()
+            if _globalvar.sanity_check(_globalvar.splitarray_to_string(filepath))==False:
+                obj.handle_error(obj.fd.feof("sanity-check-manpage-err", "Line {num}: manpage paths {sanitycheck_msg}; use spaces to denote subdirectories", num=str(obj.lineindex+1), sanitycheck_msg=_globalvar.sanity_check_error_message))
+
+            filecontent=get_file_content(filepath)
             # expect "as" clause on next line
             if obj.goto_next_line() and len(obj.lines_data[obj.lineindex].split())>0 and obj.lines_data[obj.lineindex].split()[0]=="as":
                 target_file=obj.subst_variable_content(_globalvar.splitarray_to_string(obj.lines_data[obj.lineindex].split()[1:])).split()
@@ -52,6 +71,24 @@ def handle_manpage_section(obj: _dataclass.GeneratorObject, first_phrase: str):
                 obj.write_manpage_file(target_file, filecontent, obj.lineindex+1)
             else:
                 obj.handle_error(obj.fd.feof("include-file-missing-phrase-err", "Missing \"as <filename>\" phrase on next line of line {num}", num=str(obj.lineindex+1-1)))
+        elif phrases[0]=="[include_file]":
+            obj.check_enough_args(phrases, 2)
+            filepath=obj.subst_variable_content(_globalvar.splitarray_to_string(phrases[1:])).split()
+            if _globalvar.sanity_check(_globalvar.splitarray_to_string(filepath))==False:
+                obj.handle_error(obj.fd.feof("sanity-check-manpage-err", "Line {num}: manpage paths {sanitycheck_msg}; use spaces to denote subdirectories", num=str(obj.lineindex+1), sanitycheck_msg=_globalvar.sanity_check_error_message))
+            filecontent=get_file_content(filepath)
+            while obj.goto_next_line():
+                p=obj.lines_data[obj.lineindex].split()
+                if p[0]=="as":
+                    obj.check_enough_args(p, 2)
+                    target_file=obj.subst_variable_content(_globalvar.splitarray_to_string(obj.lines_data[obj.lineindex].split()[1:])).split()
+                    if _globalvar.sanity_check(_globalvar.splitarray_to_string(target_file))==False:
+                        obj.handle_error(obj.fd.feof("sanity-check-manpage-err", "Line {num}: manpage paths {sanitycheck_msg}; use spaces to denote subdirectories", num=str(obj.lineindex+1), sanitycheck_msg=_globalvar.sanity_check_error_message))
+                    obj.write_manpage_file(target_file, filecontent, obj.lineindex+1)
+                elif p[0]=="[/include_file]":
+                    obj.check_extra_args(p, 1, use_exact_count=True)
+                    break
+                else: obj.handle_error(obj.fd.feof("invalid-phrase-err", "Unexpected \"{phrase}\" on line {num}", phrase=obj.fmt(phrases[0]), num=str(obj.lineindex+1)))
         elif phrases[0]=="set_options":
             obj.check_enough_args(phrases, 2)
             obj.handle_set_global_options(obj.subst_variable_content(_globalvar.splitarray_to_string(phrases[1:])).split())
@@ -62,3 +99,4 @@ def handle_manpage_section(obj: _dataclass.GeneratorObject, first_phrase: str):
             obj.check_extra_args(phrases, 1, use_exact_count=True)
             obj.handle_end_section("manpage")
             break
+        else: obj.handle_error(obj.fd.feof("invalid-phrase-err", "Unexpected \"{phrase}\" on line {num}", phrase=obj.fmt(phrases[0]), num=str(obj.lineindex+1)))
