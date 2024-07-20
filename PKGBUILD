c2ae1307d45c0cac6e510e9f3fe6ded6724380ec
swiftycode <10752639+swiftycode@user.noreply.gitee.com>
2024-07-20 22:02:10 +0800

    Update version (v2.0-beta2)

--- PKGBUILD
+++ PKGBUILD
@@ -1,6 +1,6 @@
 # Maintainer: swiftycode <3291929745@qq.com>
 pkgname='clitheme'
-pkgver=2.0_dev20240717
+pkgver=2.0_beta2
 pkgrel=1
 pkgdesc="A text theming library for command line applications"
 arch=('any')
--- src/clitheme/_version.py
+++ src/clitheme/_version.py
@@ -5,11 +5,11 @@ Version information definition file
 
 # Version definition file; define the package version here
 # The __version__ variable must be a literal string; DO NOT use variables
-__version__="2.0-dev20240717"
+__version__="2.0-beta2"
 major=2
 minor=0
 release=-1 # -1 stands for "dev"
 # For PKGBUILD
 # version_main CANNOT contain hyphens (-); use underscores (_) instead
-version_main="2.0_dev20240717"
+version_main="2.0_beta2"
 version_buildnumber=1
