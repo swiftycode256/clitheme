78fa321203346af636f1cce23858a5cb2eaeb084
swiftycode <10752639+swiftycode@user.noreply.gitee.com>
2024-10-24 11:22:42 +0800

    Don't retry clitheme-man if exit code is SIGINT
    Happens when ^C is pressed in the viewer

--- src/clitheme/man.py
+++ src/clitheme/man.py
@@ -64,7 +64,7 @@ def main(args: List[str]):
             except KeyboardInterrupt: process.send_signal(signal.SIGINT)
         return process.poll() # type: ignore
     returncode=run_process(env)
-    if returncode!=0 and theme_set:
+    if returncode!=0 and returncode != -signal.SIGINT and theme_set:
         _labeled_print(fd.reof("prev-command-fail", "Executing \"man\" with custom path failed, trying execution with normal settings"))
         env["MANPATH"]=prev_manpath if prev_manpath!=None else ''
         returncode=run_process(os.environ)
