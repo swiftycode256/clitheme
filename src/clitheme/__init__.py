61e539844aa01f6cf0c5ac8529496e572fbf6ca5
swiftycode <10752639+swiftycode@user.noreply.gitee.com>
2024-10-25 10:38:14 +0800

    Don't import too many modules during init
    Speed things up and avoid potential problems

--- src/clitheme/__init__.py
+++ src/clitheme/__init__.py
@@ -1,10 +1,13 @@
+"""
+Command line customization toolkit
+"""
 # Copyright © 2023-2024 swiftycode
 
 # This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
 # This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 # You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-__all__=["frontend", "cli", "man", "exec"] 
+__all__=["frontend"] 
 
 # Prevent RuntimeWarning from displaying when running a submodule (e.g. "python3 -m clitheme.exec")
 import warnings
@@ -29,6 +32,6 @@ if os.name=="nt":
 del os
 
 # Expose these modules when "clitheme" is imported
-from . import _globalvar, frontend, cli, man, exec
+from . import _globalvar, frontend
 _globalvar.handle_set_themedef(frontend, "global") # type: ignore
 del _globalvar # Don't expose this module
