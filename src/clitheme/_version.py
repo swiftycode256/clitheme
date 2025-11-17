932daf9c5c5d9f6b8a05f2297c858e9e6c0ab84a
swiftycode <10752639+swiftycode@user.noreply.gitee.com>
2025-11-17 22:30:47 +0800

    Update version (v2.1-dev20251117)

--- src/clitheme/_version.py
+++ src/clitheme/_version.py
@@ -10,7 +10,7 @@ Version information definition file
 # spell-checker:ignore buildnumber
 
 # Version definition file; define the package version here
-version_disp="2.1-dev20251116"
+version_disp="2.1-dev20251117"
 major=2
 minor=1
 # -1 stands for "dev"
@@ -20,4 +20,4 @@ beta_release=None
 # For PKGBUILD
 version_buildnumber=1
 # Build version: must be a literal string
-__version__="2.1_dev20251116"
+__version__="2.1_dev20251117"
