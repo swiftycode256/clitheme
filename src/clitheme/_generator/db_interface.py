71251676e4e59e4ca8963e2528d50c8de24ba494
swiftycode <10752639+swiftycode@user.noreply.gitee.com>
2025-12-26 23:14:15 +0800

    Move `match_content` function to `exec` module

--- src/clitheme/_generator/db_interface.py
+++ src/clitheme/_generator/db_interface.py
@@ -12,14 +12,14 @@ import sys
 import os
 import sqlite3
 import re
-import copy
 import uuid
 import gc
-from typing import Optional, List, Tuple, Dict, NamedTuple, Union
+from typing import Optional, List, Dict, NamedTuple
 from .. import _globalvar, frontend
 
 # spell-checker:ignore matchoption cmdlist exactmatch rowid pids tcpgrp nolocale
 
+match_timeout=_globalvar.output_subst_timeout
 connection=sqlite3.connect(":memory:") # placeholder
 db_path=f"{_globalvar.clitheme_root_data_path}/{_globalvar.db_filename}"
 debug_mode=False
@@ -72,7 +72,7 @@ def init_db(file_path: str):
 def connect_db(path: Optional[str]=None):
     global db_path
     if path==None: path=db_path
-    else: db_path=path
+    db_path=path
     if not os.path.exists(path):
         raise db_not_found("No theme set or theme does not contain substrules")
     global connection
@@ -144,7 +144,7 @@ def _is_db_updated() -> bool:
         return True
     else: return False
 
-def _fetch_matches(command: Optional[str]) -> List[Item]:
+def fetch_matches(command: Optional[str]) -> List[Item]:
     global _matches_cache
     updated=_is_db_updated()
     if _db_last_state==None: raise db_not_found("file at db_path does not exist")
@@ -172,7 +172,7 @@ def _get_matches(command: Optional[str]) -> List[Item]:
             for match_item in fetches:
                 # Filter based on command condition
                 if command!=None and match_item.effective_command!=None and \
-                _check_command(
+                check_command(
                     match_item.effective_command,
                     match_item.command_match_strictness,
                     command,
@@ -183,7 +183,7 @@ def _get_matches(command: Optional[str]) -> List[Item]:
 
 ## Output processing and matching
 
-def _check_command(match_cmd: str, strictness: int, target_command: str, is_regex: bool) -> bool:
+def check_command(match_cmd: str, strictness: int, target_command: str, is_regex: bool) -> bool:
     def process_smartcmdmatch_phrases(match_cmd: str) -> List[str]:
         match_cmd_phrases=[]
         for p in range(len(match_cmd.split())):
@@ -226,165 +226,3 @@ def _check_command(match_cmd: str, strictness: int, target_command: str, is_rege
             for phrase in match_cmd.split()[1:]:
                 if phrase not in target_command.split()[1:]: return False
         return True
-
-def match_content(content: bytes, command: Optional[str]=None, is_stderr: bool=False, pids: Tuple[int,int]=(-1,-1)) -> Tuple[bytes, set]:
-    # pids: (main_pid, current_tcpgrp)
-
-    content_str=copy.copy(content)
-    # Convert to str if possible
-    try: content_str=content_str.decode('utf-8')
-    except: pass
-    assert len(content_str)>0, "Empty content string"
-
-    encountered_ids=set()
-    # endmatchhere checking algorithm:
-    # - Keep track of condition mapping with same length as content
-    # - After each substitution, mark affected range in condition map as '1'
-    # - When file ID changes, reset condition mapping (re-occurring ID should never happen)
-    # -> Check if affected *lines* in the substitution contains '1'
-    if type(content_str)==bytes:
-        line_match=_globalvar.line_match_bytes
-    else: line_match=_globalvar.line_match
-    encountered_files=set()
-    last_file_id=''
-    # > \x00: not matched; \x01: matched; \x02: end match here; [other]: newline character
-    condition_map=bytearray()
-    def init_condition_map():
-        nonlocal condition_map
-        condition_map=bytearray(len(content_str))
-    init_condition_map()
-
-    for rule in _fetch_matches(command):
-        # region: Condition checking
-        if rule.unique_id in encountered_ids: continue
-        if rule.stdout_stderr_only!=0 and (is_stderr==True)+1!=rule.stdout_stderr_only: continue
-        if command!=None and rule.effective_command!=None and \
-            _check_command(
-                rule.effective_command,
-                rule.command_match_strictness,
-                command,
-                rule.command_is_regex
-            )==False: continue
-        if rule.foreground_only==True and pids[0]!=pids[1]: continue
-        # Reset endmatchhere condition map for new files
-        if rule.file_id!=last_file_id:
-            assert rule.file_id not in encountered_files, "Revisited file ID"
-            encountered_files.add(rule.file_id)
-            last_file_id=rule.file_id
-            init_condition_map()
-        # endregion
-
-        # region: Match operation
-        matched=False
-        def subst(match: re.Match) -> Union[str, bytes]:
-            nonlocal condition_map
-            # region: Check endmatchhere
-            # Determine start range: seek backward before newline is reached
-            assert len(content_str)==len(condition_map)
-            line_start=match.start()
-            for pos in range(match.start()-1, 0-1, -1):
-                ch=content_str[pos]
-                if ch not in (0,1) and content_str[pos:pos+2] not in (b'\r\n','\r\n'): # Newline character
-                    break
-                else: line_start=pos
-            # Determine end range: seek forward after newline is reached
-            line_end=match.end()
-            for pos in range(match.end(), len(content_str)+1):
-                ch=content_str[pos-1]
-                line_end=pos
-                if ch not in (0,1): # Newline character
-                    if content_str[pos-1:pos-1+2] in (b'\r\n', '\r\n'):
-                        line_end=pos+1
-                    break
-            if re.compile(b'\x02').search(condition_map, line_start, line_end)!=None:
-                return match.group() # Original string if marked sections found
-            # endregion
-
-            nonlocal sub_pattern
-            if type(content_str)==str: sub=sub_pattern
-            elif type(content_str)==bytes: sub=sub_pattern.encode('utf-8')
-            else: raise AssertionError
-            # Retrieve substituted string
-            if rule.is_regex: new_str=match.expand(sub)
-            else: new_str=sub
-            
-            # region: Update new condition map
-            # \x02 and \x01 for T/F endmatchhere condition
-            nonlocal new_condition_map, new_condition_map_offset
-            sub=bytearray([(rule.end_match_here==True)+1]*len(new_str)) # Sub pattern length
-            new_condition_map=\
-                new_condition_map[:match.start()+new_condition_map_offset] \
-                +sub \
-                +new_condition_map[match.end()+new_condition_map_offset:]
-            new_condition_map_offset+=len(sub)-(match.end()-match.start())
-            # endregion
-
-            nonlocal matched; matched=True
-            return new_str # Substituted string
-        match_pattern=rule.match_pattern if rule.is_regex else re.escape(rule.match_pattern)
-        # Replace newlines in multiline match pattern to match all possible newlines
-        # Assume: only possible newline in match pattern is '\n'
-        match_pattern=re.sub(r'\n' if rule.is_regex else r'\\\n', # '\n' is escaped in re.escape
-               rf"(?:{'|'.join(s.decode('utf-8') for s in _globalvar.newlines)})",
-               match_pattern)
-        if type(content_str)==bytes: match_pattern=match_pattern.encode('utf-8')
-        sub_pattern=rule.substitute_pattern
-
-        flags=re.MULTILINE
-        new_condition_map=condition_map
-        new_condition_map_offset=0
-        if rule.match_is_multiline:
-            # Search in entire block
-            line_lengths=[len(content_str)]
-        else:
-            # Search in individual lines
-            line_lengths=[len(m.group()) for m in re.finditer(line_match, content_str)] # type: ignore
-            # The EOL delimiter might match empty string at end of line
-            if line_lengths[-1]==0: line_lengths.pop(-1)
-        new_content=content_str
-        offset=0
-        cur_start=0
-        for length in line_lengths:
-            assert cur_start==0 or content_str[cur_start-1:cur_start] in \
-                _globalvar.newlines+tuple(s.decode('utf-8') for s in _globalvar.newlines), \
-                "Previous character is not a newline"
-            if cur_start>0:
-                # Replace previous newline character with '\n' to ensure re.MULTILINE works
-                match_str=content_str[:cur_start-1]+(b'\n' if type(content_str)==bytes else '\n')+content_str[cur_start:] # type: ignore
-            else: match_str=content_str
-            assert len(match_str)==len(content_str), \
-                f"Length mismatch: {len(match_str)}!={len(content_str)}"
-            # Perform sub on each line
-            obj_list=list(re.compile(match_pattern, flags=flags) \
-                .finditer(match_str, cur_start, cur_start+length)) # type: ignore
-            for obj in obj_list:
-                sub=subst(obj)
-                new_content=new_content[:obj.start()+offset]+sub+new_content[obj.end()+offset:] # type: ignore
-                offset+=len(sub)-(obj.end()-obj.start())
-            cur_start+=length
-        content_str=new_content
-        
-        if matched: encountered_ids.add(rule.unique_id)
-        assert len(new_condition_map)==len(content_str), \
-            f"Length mismatch: {len(condition_map)}!={len(content_str)}"
-        condition_map=new_condition_map
-    # endregion
-    # region: Check modified lines
-    line_lengths=[len(m.group()) for m in re.finditer(line_match, content_str)] # type: ignore
-    changed_line_indices=set()
-    cur_start=0
-    for x in range(len(line_lengths)):
-        length=line_lengths[x]
-        if re.compile(b'\x01|\x02').search(condition_map, cur_start, cur_start+length)!=None:
-            changed_line_indices.add(x)
-        cur_start+=length
-    # endregion
-    
-    if type(content_str)==str:
-        return (bytes(content_str, 'utf-8'), changed_line_indices)
-    elif type(content_str)==bytes:
-        return (content_str, changed_line_indices)
-    else: raise AssertionError
-
-# timeout value for each match operation
-match_timeout=_globalvar.output_subst_timeout
