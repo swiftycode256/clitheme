398552673771c23e1147bf85db46f5e526aa1038
swiftycode <10752639+swiftycode@user.noreply.gitee.com>
2025-12-27 12:49:57 +0800

    Fix header entry name processing

--- src/clitheme/_generator/_sections/header.py
+++ src/clitheme/_generator/_sections/header.py
@@ -10,31 +10,43 @@ header_section parser function (internal module)
 import re
 from ... import _globalvar
 from .. import _parser_handlers
+from typing import Optional
 
 # spell-checker:ignore infofile splitarray datapath lineindex banphrases cmdmatch minspaces blockinput optline matchoption endphrase filecontent 
 
 def handle_header_section(self: _parser_handlers.GeneratorObject, first_phrase: str):
     self.handle_begin_section("header")
     end_phrase="end_header" if first_phrase=="begin_header" else r"{/header_section}"
-    specified_info=[]
+    name_specified=True
     while self.goto_next_line():
         phrases=self.get_current_line().split()
-        if re.fullmatch(r"(name|version|description)(:)?", phrases[0])!=None:
+        last_match: Optional[re.Match]=None
+        def match_first_phrase(pattern: str) -> Optional[re.Match]:
+            # Stores the match object into a variable
+            nonlocal last_match
+            last_match=re.fullmatch(pattern, phrases[0])
+            return last_match
+        if match_first_phrase(r"(name|version|description)(:)?")!=None:
             self.check_enough_args(phrases, 2)
-            content=_globalvar.extract_content(self.get_current_line())
-            content=self.parse_content(content, pure_name=True,
-                    preserve_indents=re.fullmatch(r"(name|description)(:)?", phrases[0])!=None)
-            self.write_infofile( \
-                self.path+"/"+_globalvar.generator_info_pathname+"/"+self.custom_infofile_name, \
-                _globalvar.generator_info_filename.format(info=phrases[0]),\
-                content,self.linenum(),phrases[0]) # e.g. [...]/theme-info/1/clithemeinfo_name
-        elif re.fullmatch(r"(locales|supported_apps)(:)?", phrases[0])!=None:
+            assert last_match!=None
+            entry=last_match.group(1)
+            content=self.parse_content(_globalvar.extract_content(self.get_current_line()),
+                    pure_name=True,
+                    preserve_indents=entry in ('name', 'description'))
+            self.write_infofile(
+                self.path+"/"+_globalvar.generator_info_pathname+"/"+self.custom_infofile_name,
+                _globalvar.generator_info_filename.format(info=entry),
+                content,self.linenum(),entry) # e.g. [...]/theme-info/1/clithemeinfo_name
+            if entry=="name": name_specified=True
+        elif match_first_phrase(r"(locales|supported_apps)(:)?")!=None:
             self.check_enough_args(phrases, 2)
+            assert last_match!=None
+            entry=last_match.group(1)
             content=self.parse_content(' '.join(phrases[1:]), pure_name=True).split()
-            self.write_infofile_newlines( \
-                self.path+"/"+_globalvar.generator_info_pathname+"/"+self.custom_infofile_name, \
-                _globalvar.generator_info_v2filename.format(info=phrases[0]),\
-                content,self.linenum(),phrases[0]) # e.g. [...]/theme-info/1/clithemeinfo_description_v2
+            self.write_infofile_newlines(
+                self.path+"/"+_globalvar.generator_info_pathname+"/"+self.custom_infofile_name,
+                _globalvar.generator_info_v2filename.format(info=entry),
+                content,self.linenum(),entry) # e.g. [...]/theme-info/1/clithemeinfo_description_v2
         elif phrases[0] in ("[locales]", "[supported_apps]", "[description]", "locales_block", "supported_apps_block", "description_block"):
             self.check_extra_args(phrases, 1)
             # handle block input
@@ -47,16 +59,15 @@ def handle_header_section(self: _parser_handlers.GeneratorObject, first_phrase: 
                         if is_description else \
                         _globalvar.generator_info_v2filename) \
                 .format(info=re.sub(r'_block$', '', phrases[0]).replace('[','').replace(']',''))
-            self.write_infofile( \
-                self.path+"/"+_globalvar.generator_info_pathname+"/"+self.custom_infofile_name, \
-                file_name,\
+            self.write_infofile(
+                self.path+"/"+_globalvar.generator_info_pathname+"/"+self.custom_infofile_name,
+                file_name,
                 content,self.linenum(),re.sub(r'_block$','',phrases[0])) # e.g. [...]/theme-info/1/clithemeinfo_description_v2
         elif self.handle_setters(): pass
         elif phrases[0]==end_phrase:
             self.check_extra_args(phrases, 1)
-            if not ("name" in specified_info or "name:" in specified_info):
+            if not name_specified:
                 self.handle_error(self.fd.feof("missing-info-err", "{sect_name} section missing required entries: {entries}", sect_name="header", entries="name"))
             self.handle_end_section("header")
             break
         else: self.handle_invalid_phrase(phrases[0])
-        specified_info.append(phrases[0])
