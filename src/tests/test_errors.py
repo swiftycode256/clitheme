4e733ab4d137b279335fa30005006bfd5090ad77
swiftycode <10752639+swiftycode@user.noreply.gitee.com>
2026-01-12 22:46:04 +0800

    Add expected error messages to tests

--- src/tests/test_errors.py
+++ src/tests/test_errors.py
@@ -8,9 +8,10 @@ import os
 import sys
 import shutil
 import re
-from typing import Optional
+import difflib
+from typing import Optional, Dict, List
 sys.path=[f"{os.path.dirname(__file__)}/.."]+sys.path
-from clitheme import _generator, _globalvar
+from clitheme import _generator, frontend, _globalvar
 import unittest
 import warnings
 
@@ -18,10 +19,22 @@ class TestErrors(unittest.TestCase):
     def setUp(self):
         print()
         warnings.simplefilter("ignore")
-        self.generator_path: Optional[str]=None
-    def tearDown(self):
-        if self.generator_path!=None:
-            shutil.rmtree(self.generator_path)
+        # Ensure localization settings are correct
+        _globalvar.handle_set_themedef("test")
+    def _run_test(self, test_file: str, expected_msgs: Dict[str, List[str]]):
+        for lang, lines in expected_msgs.items():
+            frontend.global_lang=lang
+            _globalvar.msg_retrieved=False # For sanity check messages
+            return_val=_generator.generate_data_hierarchy(test_file)
+            generator_path=return_val.dir_path
+            self.assertEqual(return_val.success, False)
+            self.assertGreater(len(return_val.messages), 0)
+            shutil.rmtree(generator_path)
+
+            print('\n'.join(return_val.messages))
+            if return_val.messages!=lines:
+                print('\n'+'\n'.join(difflib.ndiff(lines, return_val.messages)))
+                self.fail("Messages not equal")
     def test_errors(self):
         filepath=re.sub(r'[\\/]', ' ', os.path.relpath(__file__))
         test_file=rf"""
@@ -48,7 +61,9 @@ class TestErrors(unittest.TestCase):
             <filter_cmd_regex> (
             # bad-match-pattern-err
             [subst_regex] )]
-                # (Errors not shown if bad match pattern)
+                # phrase-format-err
+                locale[]: this
+                # (Pattern errors not shown if bad match pattern)
                 default: \g
             [/subst_regex]
             [subst_regex] this
@@ -99,11 +114,67 @@ class TestErrors(unittest.TestCase):
         # unterminated-section-err
         {header}
         """
-        return_val=_generator.generate_data_hierarchy(test_file)
-        self.generator_path=return_val.dir_path
-        self.assertEqual(return_val.success, False)
-        self.assertGreater(len(return_val.messages), 0)
-        print("\n".join(return_val.messages))
+        c=0
+        def next(n: int): nonlocal c; c+=n; return c
+        expected_msgs={}
+        expected_msgs['en_US']= [
+            f"Error: Line {next(6)}: Invalid line boundary format",
+            f"Error: header section missing required entries: name",
+            f"Error: Line {next(5)}: Header macro \"!require_version\" must be specified before other lines",
+            f"Error: Line {next(3)}: Unknown option \"wef\"",
+            f"Error: Line {next(0)}: No value specified for option \"leadspaces\"",
+            f"Error: Line {next(0)}: The value specified for option \"leadtabindents\" is not an integer",
+            f"Error: Line {next(0)}: The option \"exactcmdmatch\" can't be set at the same time with \"strictcmdmatch\"",
+            f"Error: Line {next(2)}: Option \"linebounds\" not allowed here",
+            f"Error: Line {next(0)}: \"ESC\" is not a valid variable name",
+            f"Error: Line {next(0)}: \"{{\" is not a valid variable name",
+            f"Error: Line {next(2)}: Invalid format for \"setvar\"",
+            f"Error: Line {next(4)}: Bad command filter pattern (missing ), unterminated subpattern at position 0)",
+            f"Error: Line {next(2)}: Bad match pattern (unbalanced parenthesis at position 0)",
+            f"Error: Line {next(2)}: Invalid format for \"locale\"",
+            f"Error: Line {next(6)}: Bad substitute pattern (missing < at position 2)",
+            f"Error: Line {next(1)}: Bad substitute pattern (invalid group reference 2 at position 3)",
+            f"Error: Line {next(10)}: Cannot create subsection \"this\" because an entry with the same name already exists",
+            f"Error: Line {next(3)}: Cannot create subsection \"this\" because an entry with the same name already exists",
+            f"Error: Line {next(9)}: Cannot create entry \"another\" because a subsection with the same name already exists",
+            f"Error: Line {next(3)}: Domain and app names cannot start with '.'",
+            f"Error: Line {next(2)}: Subsection names cannot contain '?'",
+            f"Error: Line {next(2)}: Entry subsections/names cannot contain '*'",
+            f"Error: Line {next(7)}: Missing \"as <filename>\" phrase on next line",
+            f"Error: Line {next(7)}: Repeated header section",
+            f"Error: Unterminated header section at end of file",
+            f"Error: Missing or incomplete header or content sections",
+        ]
+        c=0
+        expected_msgs['zh_CN']=[
+            f"错误：第{next(6)}行：无效的行边界格式",
+            f"错误：header段落缺少必要条目：name",
+            f"错误：第{next(5)}行：头定义\"!require_version\"必须在其他行之前声明",
+            f"错误：第{next(3)}行：未知选项\"wef\"",
+            f"错误：第{next(0)}行：选项\"leadspaces\"未指定数值",
+            f"错误：第{next(0)}行：选项\"leadtabindents\"指定的数值不是整数",
+            f"错误：第{next(0)}行：选项\"exactcmdmatch\"和\"strictcmdmatch\"不能同时指定",
+            f"错误：第{next(2)}行：选项\"linebounds\"不允许在这里指定",
+            f"错误：第{next(0)}行：\"ESC\"不是一个有效的变量名称",
+            f"错误：第{next(0)}行：\"{{\"不是一个有效的变量名称",
+            f"错误：第{next(2)}行：无效的\"setvar\"格式",
+            f"错误：第{next(4)}行：无效的命令限制正则表达式（missing ), unterminated subpattern at position 0）",
+            f"错误：第{next(2)}行：无效的匹配正则表达式（unbalanced parenthesis at position 0）",
+            f"错误：第{next(2)}行：无效的\"locale\"格式",
+            f"错误：第{next(6)}行：无效的替换正则表达式（missing < at position 2）",
+            f"错误：第{next(1)}行：无效的替换正则表达式（invalid group reference 2 at position 3）",
+            f"错误：第{next(10)}行：无法创建子路径\"this\"，因为拥有相同名称的定义已存在",
+            f"错误：第{next(3)}行：无法创建子路径\"this\"，因为拥有相同名称的定义已存在",
+            f"错误：第{next(9)}行：无法创建定义\"another\"，因为拥有相同名称的子路径已存在",
+            f"错误：第{next(3)}行：开发者和应用程序名称不能以'.'开头",
+            f"错误：第{next(2)}行：子路径名称不能包含'?'",
+            f"错误：第{next(2)}行：定义路径名称不能包含'*'",
+            f"错误：第{next(7)}行：在下一行缺少\"as <文件名>\"语句",
+            f"错误：第{next(7)}行：重复的header段落",
+            f"错误：在文件结尾未结束header段落",
+            f"错误：文件缺少或包含不完整的header或内容段落",
+        ]
+        self._run_test(test_file, expected_msgs)
 
 # Syntax errors:
 # invalid-version-err
